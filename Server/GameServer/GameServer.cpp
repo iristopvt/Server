cc99dd9b5ea976efe55c1d5462967150fdcc961d
iristopvt <iristopvt@naver.com>
2024-10-11 00:27:49 +0900

    test

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,6 +1,42 @@
 ﻿#include "pch.h"
 
+#include "AccountManager.h"
+#include "UserManager.h"
+
+#include "ThreadManager.h"
+#include "Lock.h"
+
+void Login()
+{
+	while(true)
+		AccountManager::GetInstance()->Login();
+}
+
+void Save()
+{
+	while(true)
+		UserManager::GetInstance()->Save();
+}
+
 int main()
 {
-    HelloWorld();
+	CoreGlobal::Create();
+
+	AccountManager::Create();
+	UserManager::Create();
+
+
+	// DeadLock
+	TM_M->Launch(Login);
+	TM_M->Launch(Save);
+
+	TM_M->Join();
+
+
+	AccountManager::Delete();
+	UserManager::Delete();
+
+	CoreGlobal::Delete();
+
+	return 0;
 }
--- Server/ServerCore/CorePch.h
+++ Server/ServerCore/CorePch.h
@@ -3,16 +3,22 @@
 #include "Types.h"
 #include <vector>
 #include <list>
-#include <queue>
-#include <stack>
 #include <unordered_map>
 #include <map>
 #include <set>
 #include <unordered_set>
 #include <algorithm>
+#include <future>
 
+#include <Windows.h>
 #include <iostream>
 
 using namespace std;
 
-void HelloWorld();
+// CORE
+#include "CoreTLS.h"
+#include "CoreGlobal.h"
+#include "CoreMacro.h"
+
+// Multy Thread
+#include "Lock.h"
